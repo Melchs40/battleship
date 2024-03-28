6fdd078e5c17528d19999ad85433e1d93ed9d7bf
Melchs40 <samscodingjourney@gmail.com>
2024-03-28 17:42:19 -0500

    Create style.scss, style.css, style.css.map and edit index.js to begin development for user interface

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+import './style.css';
 
+let content = document.getElementById('content');
+let background = document.createElement('div');
+background.classList.add('background');
+
+let firstCircle = document.createElement('div');
+firstCircle.classList.add('circle');
+firstCircle.setAttribute('id', 'first');
+
+let secondCircle = document.createElement('div');
+secondCircle.classList.add('circle');
+secondCircle.setAttribute('id', 'second');
+
+let thirdCircle = document.createElement('div');
+thirdCircle.classList.add('circle');
+thirdCircle.setAttribute('id', 'third');
+
+content.appendChild(background);
+background.appendChild(firstCircle);
+firstCircle.appendChild(secondCircle);
+secondCircle.appendChild(thirdCircle);
+
+console.log('hello');
