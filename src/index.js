bff3871fc4b934ec7671805721c9f31b057f0806
Melchs40 <samscodingjourney@gmail.com>
2024-03-28 22:49:45 -0500

    Create animated radar background in style.scss with DOM elements from index.js

--- src/index.js
+++ src/index.js
@@ -16,9 +16,31 @@ let thirdCircle = document.createElement('div');
 thirdCircle.classList.add('circle');
 thirdCircle.setAttribute('id', 'third');
 
+let fourthCircle = document.createElement('div');
+fourthCircle.classList.add('circle');
+fourthCircle.setAttribute('id', 'fourth');
+
+let fifthCircle = document.createElement('div');
+fifthCircle.classList.add('circle');
+fifthCircle.setAttribute('id', 'fifth');
+
+let vLine = document.createElement('div');
+vLine.setAttribute('id', 'v-line');
+
+let hLine = document.createElement('div');
+hLine.setAttribute('id', 'h-line');
+
+let moveLine = document.createElement('div');
+moveLine.setAttribute('id', 'move-line');
+
 content.appendChild(background);
 background.appendChild(firstCircle);
 firstCircle.appendChild(secondCircle);
 secondCircle.appendChild(thirdCircle);
+thirdCircle.appendChild(fourthCircle);
+fourthCircle.appendChild(fifthCircle);
+firstCircle.appendChild(vLine);
+firstCircle.appendChild(hLine);
+firstCircle.appendChild(moveLine);
 
 console.log('hello');
